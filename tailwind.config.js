148fcbdb7a65645a110fc9c57bc57a1f0da178df
Gabriel <gsamascarenhas@gmail.com>
2022-11-10 22:57:37 -0300

    refactor: adicionar cor gray-550 ao tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       colors: {
         "gray-10": "#F0F1F9",
+        "gray-550": "#606060",
         "orange-650": "#FE2F01",
         "violet-550": "#5458BE",
       },
