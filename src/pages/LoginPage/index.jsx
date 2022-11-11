8f55d7284cf5e3ccab36f97f850144c521bb4e8f
Gabriel <gsamascarenhas@gmail.com>
2022-11-10 23:17:50 -0300

    feat:finalizar login page

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,3 +1,96 @@
+import logoImg from "../../images/laranja_logo.png";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { FcGoogle } from "react-icons/fc";
+
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Digite um email válido")
+      .required("Email obrigatório"),
+    password: yup.string().required("Digite sua senha"),
+  })
+  .required();
+
 export function LoginPage() {
-  return <h1 className="text-5xl text-orange-700">LoginPage</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div
+      id="container"
+      className="px-4 pt-10 flex flex-col items-center max-w-xs mx-auto"
+    >
+      <img src={logoImg} alt="logo" />
+      <h2 className="font-bold mt-8 text-xl">Login</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+        <label
+          htmlFor="email"
+          className="block ml-2 mt-16 mb-1 font-bold text-sm"
+        >
+          Email
+        </label>
+        <input
+          type="text"
+          placeholder="exemplo@gmail.com"
+          className={`border-[0.3px] border-[#ADADAD] ${
+            errors.email && "border-red-600"
+          }  bg-gray-10 rounded w-full text-sm p-3`}
+          {...register("email")}
+        />
+        <p className="text-red-600">{errors.email?.message}</p>
+
+        <label
+          htmlFor="password"
+          className="block ml-2 mt-5 mb-1 font-bold text-sm"
+        >
+          Senha
+        </label>
+        <input
+          type="password"
+          placeholder="**********"
+          className={`border-[0.3px] border-[#ADADAD] ${
+            errors.password && "border-red-600"
+          }  bg-gray-10 rounded w-full text-xs p-3`}
+          {...register("password")}
+        />
+        <p className="text-red-600">{errors.password?.message}</p>
+        <p className="text-right text-xs font-extrabold mt-2 text-gray-550">
+          Esqueçeu a senha?
+        </p>
+        <input
+          type="submit"
+          className="text-lg font-bold text-white bg-orange-650 w-full rounded 
+                     py-3 px-16 mt-4 cursor-pointer"
+          value="Entrar"
+        />
+      </form>
+      <div id="ou" className="flex items-center mt-4">
+        <hr className="border-t-[1px] w-24 border-black opacity-30" />
+        <p className="mx-8">Ou</p>
+        <hr className="border-t-[1px] w-24 border-black opacity-30" />
+      </div>
+      <button
+        className="flex items-center justify-center gap-4 mt-5 py-4 rounded border-[2px] 
+        border-black border-opacity-30 text-sm font-medium w-full"
+      >
+        <FcGoogle size={20} />
+        Login com o Google
+      </button>
+      <p className="text-xs font-normal mt-7">
+        Não tem conta ainda?
+        <a href="/" className="font-extrabold underline">
+          Cadastre-se
+        </a>
+      </p>
+    </div>
+  );
 }
