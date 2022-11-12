a17761eeed0f2c3921cade711b7444f34fc0034f
Gabriel <gsamascarenhas@gmail.com>
2022-11-11 23:00:46 -0300

    refactor:remover margin top do component Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,25 +1,22 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Button = ({title, onClick}) => {
-    return ( 
-        <ButtonElement
-            className='bg-orange-650'
-            onClick={onClick}
-        >{title}</ButtonElement>
-     );
-}
- 
+const Button = ({ title, onClick }) => {
+  return (
+    <ButtonElement className="bg-orange-650" onClick={onClick}>
+      {title}
+    </ButtonElement>
+  );
+};
+
 export default Button;
 
 const ButtonElement = styled.button`
-    color: white;
-    font-size: 1.125rem;
-    line-height: 1.75rem;
-    font-weight: 700;
-    border-radius: 0.25rem;
-    width: 100%;
-    margin-top: 1rem;
-    padding-top: 0.75rem;
-    padding-bottom: 0.75rem;
-
-`
+  color: white;
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+  font-weight: 700;
+  border-radius: 0.25rem;
+  width: 100%;
+  padding-top: 0.75rem;
+  padding-bottom: 0.75rem;
+`;
