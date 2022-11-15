08eca48130cd3985b3ccf9e5ff5ff4506d06adc4
anti-duhring <mateusvnlima@gmail.com>
2022-11-15 05:01:04 -0300

    feat: Fluxo de autenticação nas rotas

--- src/components/Navbar/Sidebar/index.jsx
+++ src/components/Navbar/Sidebar/index.jsx
@@ -8,10 +8,15 @@ import { GiExitDoor } from "react-icons/gi";
 import { FaGraduationCap } from 'react-icons/fa';
 import { Links } from '../../../constants/links';
 import styled from "styled-components";
+import useAuthStore from "../../../context/authStore";
+import { BsShieldFillExclamation } from 'react-icons/bs';
+import { useLogout } from '../../../hooks/useLogout';
+import { RiLoginCircleFill } from 'react-icons/ri';
+import { BsFillPersonPlusFill } from 'react-icons/bs'
 
 import "./index.css";
 
-export function Sidebar() {
+const NotAuth = () => {
   return (
     <Menu>
       <Link
@@ -21,6 +26,67 @@ export function Sidebar() {
         <BsHouse />
         <span>Página Inicial</span>
       </Link>
+      <Link
+        className="menu-item"
+        href={Links.path.login}
+      >
+        <RiLoginCircleFill />
+        <span>Entrar</span>
+      </Link>
+      <Link
+        className="menu-item"
+        href={Links.path.register}
+      >
+        <BsFillPersonPlusFill />
+        <span>Registrar</span>
+      </Link>
+      <Link
+        className="menu-item"
+        href="https://discord.com/channels/847518545156112424"
+        target='_blank'
+      >
+        <MdModeComment />
+        <span>Comunidade</span> 
+      </Link>
+      <Link
+        className="menu-item"
+        href="#"
+      >
+        <AiFillQuestionCircle />
+        <span>Dúvidas</span> 
+      </Link>
+      <hr className="divisao" />
+      <Link
+        className="menu-item"
+        href="#"
+      >
+        <MdOutlineLightbulb />
+        <span>Apagas as luzes</span> 
+      </Link>
+    </Menu>
+  )
+}
+const Auth = () => {
+  const { user } = useAuthStore((state) => ({ user: state.user }))
+  const logout = useLogout();
+
+  return (
+    <Menu>
+      <Link
+        className="menu-item"
+        href="/"
+      >
+        <BsHouse />
+        <span>Página Inicial</span>
+      </Link>
+      {user?.admin && 
+      <Link
+        className="menu-item"
+        href={Links.path.admin.root}
+      >
+        <BsShieldFillExclamation />
+        <span>Administração</span>
+      </Link>}
       <Link
         className="menu-item"
         href={Links.courses.root}
@@ -60,12 +126,22 @@ export function Sidebar() {
       </Link>
       <Link
         className="menu-item"
-        href="/"
+        href="#"
+        onClick={logout}
       >
         <GiExitDoor />
         <span>Sair</span> 
       </Link>
     </Menu>
+  )
+}
+export function Sidebar() {
+  const { user } = useAuthStore((state) => ({ user: state.user }))
+
+  return (
+    <>
+      {user? <Auth /> : <NotAuth />}
+    </>
   );
 }
 
